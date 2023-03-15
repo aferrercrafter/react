09599a25f816c50c242cb17164840f4a74d042ca
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2023-03-15 17:49:48 -0300

    added winner logic

--- react-tic-tac/src/App.js
+++ react-tic-tac/src/App.js
@@ -11,16 +11,30 @@ function Square({value, handleSquareLink}) {
 }
 
 export default function Board() {
+  const [XisNext, setXisNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick(i) {
+    if(squares[i] || calculateWinner(squares)) return
+
     const newSquareState = squares.slice()
-    newSquareState[i] = 'X'
+    newSquareState[i] = XisNext? 'X' : 'O'
+
+    setXisNext(!XisNext)
     setSquares(newSquareState)
   }
 
+  const winner = calculateWinner(squares)
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (XisNext ? "X" : "O");
+    }
+
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} handleSquareLink = {() => handleClick(0)}/>
       <Square value={squares[1]} handleSquareLink = {() => handleClick(1)}/>
@@ -38,4 +52,24 @@ export default function Board() {
     </div>
   </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
