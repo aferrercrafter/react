176add32ce1e5e69d13e6ded0c77886d87b32951
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2023-03-15 17:33:52 -0300

    passed state management to Board

--- react-tic-tac/src/App.js
+++ react-tic-tac/src/App.js
@@ -1,37 +1,40 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null)
-
-  function handleClick() {
-    setValue('X')
-  }
+function Square({value, handleSquareLink}) {
 
   return <button 
     className="square"
-    onClick={handleClick}
+    onClick={handleSquareLink}
     >
       {value}
     </button>;
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick(i) {
+    const newSquareState = squares.slice()
+    newSquareState[i] = 'X'
+    setSquares(newSquareState)
+  }
+
   return (
     <>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[0]} handleSquareLink = {() => handleClick(0)}/>
+      <Square value={squares[1]} handleSquareLink = {() => handleClick(1)}/>
+      <Square value={squares[2]} handleSquareLink = {() => handleClick(2)}/>
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]} handleSquareLink = {() => handleClick(3)}/>
+      <Square value={squares[4]} handleSquareLink = {() => handleClick(4)}/>
+      <Square value={squares[5]} handleSquareLink = {() => handleClick(5)}/>
     </div>
     <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]} handleSquareLink = {() => handleClick(6)}/>
+      <Square value={squares[7]} handleSquareLink = {() => handleClick(7)}/>
+      <Square value={squares[8]} handleSquareLink = {() => handleClick(8)}/>
     </div>
   </>
   );
